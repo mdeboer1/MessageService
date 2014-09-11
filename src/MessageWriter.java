16c7120bf5e0255a9766d7e04a0d4e335f0da4e6
markr_000 <markr_000@Jet>
2014-09-11 15:21:01 -0500

    Created project and both interfaces for the reading and writing of the messages.

---
+++ src/MessageWriter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mdeboer1
+ * This is the interface for reading messages intended to be compliant with
+ * DIP and Liskov's Substitution Principle
+ */
+public interface MessageWriter {
+    
+    public abstract void writeMessage(String message);
+}
