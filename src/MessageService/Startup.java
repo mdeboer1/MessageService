fad2058885e6bd0be22c8d3ac8b4c3b7e350c14a
markr_000 <markr_000@Jet>
2014-09-15 15:41:23 -0500

    Modified Startup to give user warning message when not entering in the correct number from the available options.

--- src/MessageService/Startup.java
+++ src/MessageService/Startup.java
@@ -20,18 +20,27 @@ public class Startup {
 
             
             String read, write;
+                      
+            read = JOptionPane.showInputDialog("How would you like to input the message?" +
+                        "Type 1 for console input, 2 for GUI input, or 3 to get from an array.");
             
-            // Loops ask for user input then validate the input so it only allows in what we want.
-            do {
+            while( !((read.equals("1") || read.equals("2") || read.equals("3")))){
+                //display warning message
+                JOptionPane.showMessageDialog(null, "You have entered a non-supported number.",
+                        "Warning", JOptionPane.WARNING_MESSAGE);
                 read = JOptionPane.showInputDialog("How would you like to input the message?" +
                         "Type 1 for console input, 2 for GUI input, or 3 to get from an array.");
-            } while( !((read.equals("1") || read.equals("2") || read.equals("3"))));
+            }
             
-            do {
-                write = JOptionPane.showInputDialog("How would you like to display your message?" +
+            write = JOptionPane.showInputDialog("How would you like to display your message?" +
                     "Type 1 to display in the console or 2 to display in a GUI.");
-            } while (!(write.equals("1") || write.equals("2")));
             
+            while (!(write.equals("1") || write.equals("2"))){
+                    JOptionPane.showMessageDialog(null, "You have entered a non-supported number.",
+                        "Warning", JOptionPane.WARNING_MESSAGE);
+                    write = JOptionPane.showInputDialog("How would you like to display your message?" +
+                    "Type 1 to display in the console or 2 to display in a GUI.");
+            }        
             MessageManager manager = new MessageManager(write, read);
             manager.manageMessage();
         }   
